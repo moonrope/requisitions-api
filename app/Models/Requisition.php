d66207a26fd4b903c73cc719626d5a2d613d94d1
Miguel Aguirre <miguel@brainbase.com>
2022-04-11 21:50:52 +0200

    Add requisition model

--- app/Models/Requisition.php
+++ app/Models/Requisition.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use App\Traits\TraitUuid;
+use Database\Factories\RequisitionFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Requisition extends Model
 {
-    use HasFactory;
+    use TraitUuid, HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'description'
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return Factory
+     */
+    protected static function newFactory(): Factory
+    {
+        return RequisitionFactory::new();
+    }
 }
