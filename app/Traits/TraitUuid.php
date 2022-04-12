e2f7f525e2f45643da920cfe85f4aa27a17caa68
Miguel Aguirre <miguel@brainbase.com>
2022-04-12 12:15:43 +0200

    Refactor trait

--- app/Traits/TraitUuid.php
+++ app/Traits/TraitUuid.php
@@ -14,36 +14,14 @@ trait TraitUuid
     {
         parent::boot();
 
-        $creationCallback = function ($model) {
-            if (empty($model->uuid))
+        $uuid = static::$uuidFieldName ?? 'uuid';
+        $creationCallback = function ($model) use($uuid) {
+            if (empty($model->{$uuid}))
             {
-                $model->uuid = Str::uuid()->toString();
+                $model->{$uuid} = Str::uuid()->toString();
             }
         };
 
         static::creating($creationCallback);
-    }
-
-
-    /**
-     * Override the getIncrementing() function to return false to tell
-     * Laravel that the identifier does not auto increment (it's a string).
-     *
-     * @return bool
-     */
-    public function getIncrementing() : bool
-    {
-        return false;
-    }
-
-
-    /**
-     * Tell laravel that the key type is a string, not an integer.
-     *
-     * @return string
-     */
-    public function getKeyType() : string
-    {
-        return 'string';
     }
 }
